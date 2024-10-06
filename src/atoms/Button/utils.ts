ccb5b8ddc8a309a348175c3a607940fc31821f39
Isaac Cambronero <jicv8@outlook.com>
2024-10-06 02:06:26 -0600

    Styles fixes and wrong date fix

--- src/atoms/Button/utils.ts
+++ src/atoms/Button/utils.ts
@@ -2,7 +2,7 @@ import { EButtonType } from "./interfaces";
 import { buttonTypeStyles } from "./variables";
 
 export function getStyles(type?: EButtonType) {
-  const generalStyles = "px-4 py-2 rounded-lg";
+  const generalStyles = "px-4 py-2 rounded-lg min-w-fit";
   const selectedStyles = buttonTypeStyles[type || EButtonType.CONTAINED];
 
   if (!selectedStyles) return generalStyles;
