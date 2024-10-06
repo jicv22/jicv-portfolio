f48058a6c042a2961374c2b0b83dcdff7d4c0696
Isaac Cambronero <jicv8@outlook.com>
2024-10-06 02:13:59 -0600

    TextWithHighlights Component

---
+++ src/molecules/TextWithHighlights/TextWithHighlights.tsx
@@ -0,0 +1,16 @@
+import { ETextMode, ETextType, Text } from "../../atoms";
+import { ITextWithHighlightsParam } from "./interfaces";
+
+function TextWithHighlights({ texts }: ITextWithHighlightsParam) {
+  return texts.map((item, index) => (
+    <Text
+      key={index}
+      type={ETextType.T3}
+      mode={item.highlight ? ETextMode.HIGHLIGHTED : undefined}
+    >
+      {item.text}
+    </Text>
+  ));
+}
+
+export default TextWithHighlights;
