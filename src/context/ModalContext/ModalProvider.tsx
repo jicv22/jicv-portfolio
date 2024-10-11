088845d5f5b7c69b5801a20f1f782244a54ff233
Isaac Cambronero <jicv8@outlook.com>
2024-10-10 19:10:19 -0600

    modal context, hook, template

---
+++ src/context/ModalContext/ModalProvider.tsx
@@ -0,0 +1,31 @@
+import { ReactNode, useState } from "react";
+import ModalContext from "./ModalContext";
+import { ModalTemplate } from "../../templates";
+
+export function ModalProvider({ children }: { children: ReactNode }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalContent, setModalContent] = useState<ReactNode | null>(null);
+
+  const openModal = (modalComponent: ReactNode) => {
+    setModalContent(modalComponent);
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+    setModalContent(null);
+  };
+
+  return (
+    <ModalContext.Provider
+      value={{ isOpen, openModal, closeModal, modalContent }}
+    >
+      {children}
+      {isOpen && (
+        <ModalTemplate>{modalContent}</ModalTemplate>
+      )}
+    </ModalContext.Provider>
+  );
+}
+
+export default ModalProvider;
