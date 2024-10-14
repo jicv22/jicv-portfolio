054bfd8da519f8807bc70d57b954d08e49c2c766
Isaac Cambronero <jicv8@outlook.com>
2024-10-14 08:14:31 -0600

    Base close button

--- src/templates/ModalTemplate/ModalTemplate.tsx
+++ src/templates/ModalTemplate/ModalTemplate.tsx
@@ -1,3 +1,4 @@
+import { Button, EButtonType, ETextType, Text } from "../../atoms";
 import useModal from "../../hooks/useModal";
 import { PageTemplate } from "../PageTemplate";
 import { IModalTemplateParam } from "./interfaces";
@@ -5,22 +6,27 @@ import { IModalTemplateParam } from "./interfaces";
 function ModalTemplate({ children }: IModalTemplateParam) {
   const { closeModal } = useModal();
 
-  const handleClickOutside = (event: React.MouseEvent<HTMLDivElement>) => {
+  function handleClickOutside(event: React.MouseEvent<HTMLDivElement>) {
     const target = event.target as HTMLElement;
     if (target.closest("#modal-outside") === null) {
       closeModal();
     }
-  };
+  }
 
   return (
     <div
-      className="z-50 fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm px-52 py-24"
+      className="z-50 fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm sm:px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-52 sm:py-12 lg:py-24"
       onClick={handleClickOutside}
     >
       <div
         id="modal-child-container"
-        className="max-w-full max-h-full rounded-3xl overflow-x-clip overflow-y-auto scrollbar-thin scrollbar-thumb-darkGray scrollbar-track-transparent"
+        className="relative max-w-full max-h-full rounded-3xl overflow-x-clip overflow-y-auto scrollbar-thin scrollbar-thumb-darkGray scrollbar-track-transparent"
       >
+        <div className="z-50 absolute w-8 h-8 flex top-2 right-1 lg:invisible">
+          <Button grow type={EButtonType.CONTENT_ONLY} onClick={closeModal}>
+            <Text type={ETextType.T1}>x</Text>
+          </Button>
+        </div>
         <PageTemplate isPageUp>{children}</PageTemplate>
       </div>
     </div>
