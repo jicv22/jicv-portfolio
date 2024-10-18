bee6889428859c033832945aa1b1add362427cac
Isaac Cambronero <jicv8@outlook.com>
2024-10-18 03:06:25 -0600

    Components Movement

--- src/templates/ModalTemplate/interfaces.ts
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from "react";
-
-export interface IModalTemplateParam {
-  children: ReactNode;
-}
--- src/templates/index.ts
+++ src/templates/index.ts
@@ -1,5 +1,1 @@
 export * from "./PageTemplate";
-export * from "./ModalTemplate";
-export * from "./Carousel";
-export * from "./List";
-export * from "./TitledComponent";
