72fedb20436f514aaf36f826938031686733612e
Isaac Cambronero <jicv8@outlook.com>
2024-10-17 19:38:41 -0600

    Better functional components

--- src/templates/Carousel/Carousel.tsx
+++ src/templates/Carousel/Carousel.tsx
@@ -1,11 +1,10 @@
+import { FC } from "react";
 import { ICarouselParam } from "./interfaces";
 
-function Carousel({ children }: ICarouselParam) {
-  return (
-    <div className="flex space-x-4 max-w-full overflow-x-auto py-3 px-1">
-      {children}
-    </div>
-  );
-}
+const Carousel: FC<ICarouselParam> = ({ children }) => (
+  <div className="flex space-x-4 max-w-full overflow-x-auto py-3 px-1">
+    {children}
+  </div>
+);
 
 export default Carousel;
--- src/templates/List/List.tsx
+++ src/templates/List/List.tsx
@@ -1,18 +1,18 @@
+import React, { ReactNode, FC } from "react";
 import { IListParam } from "./interfaces";
 
-function List({ children }: IListParam) {
-  return (
-    <ul className="list-none pl-6">
-      {children.map((child, index) => (
-        <li
-          key={`li-${index}`}
-          className="relative text-white before:content-['●'] before:absolute before:-left-4 before:top-1/2 before:-translate-y-1/2 before:text-white before:text-lg"
-        >
-          {child}
-        </li>
-      ))}
-    </ul>
-  );
-}
+const ListItem: FC<{ children: ReactNode }> = ({ children }) => (
+  <li className="relative text-white before:content-['●'] before:absolute before:-left-4 before:top-1/2 before:-translate-y-1/2 before:text-white before:text-lg">
+    {children}
+  </li>
+);
+
+const List: FC<IListParam> = ({ children }) => (
+  <ul className="list-none pl-6">
+    {React.Children.map(children, (child, index) => (
+      <ListItem key={`li-${index}`}>{child}</ListItem>
+    ))}
+  </ul>
+);
 
 export default List;
