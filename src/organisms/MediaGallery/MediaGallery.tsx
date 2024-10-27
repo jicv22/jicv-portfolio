fc02fec2942b7cb20da3abb2df66010eb25e8dbf
Isaac Cambronero <jicv8@outlook.com>
2024-10-27 09:37:46 -0600

    Media gallery img selector responsiveness

--- src/organisms/MediaGallery/MediaGallery.tsx
+++ src/organisms/MediaGallery/MediaGallery.tsx
@@ -19,7 +19,7 @@ const MediaGallery: FC<IMediaGalleryParam> = ({ mediaContent }) => {
         {mediaContent.map((media, index) => (
           <img
             key={`mdcnt-${index}`}
-            className="w-24 aspect-square object-cover cursor-pointer"
+            className="w-2/12 sm:w-20 md:w-24 aspect-square object-cover cursor-pointer"
             src={media.url}
             alt="Project Image"
             onClick={() => onThumbnailClicked(index)}
