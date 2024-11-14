5376bed381caf8f80ceae92f8991e7303339eab4
Isaac Cambronero <jicv8@outlook.com>
2024-11-14 13:28:34 -0600

    undesired color override fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,20 +9,17 @@ export default {
       textShadow: {
         white: "0px 0px 4px rgba(255, 255, 255, .5)",
       },
-    },
-    colors: {
-      electricBlue: "#007BFF",
-      darkElectricBlue: "#0A62BF",
-      lightGray: "#E0E0E0",
-      darkGray: "#262626",
-      darkDarkGray: "#0A0A0A",
-      black: "#000000",
-      white: "#FFFFFF",
-      transparent: 'rgba(0, 0, 0, 0)'
+      colors: {
+        electricBlue: "#007BFF",
+        darkElectricBlue: "#0A62BF",
+        lightGray: "#E0E0E0",
+        darkGray: "#262626",
+        darkDarkGray: "#0A0A0A",
+        black: "#000000",
+        white: "#FFFFFF",
+        transparent: "rgba(0, 0, 0, 0)",
+      },
     },
   },
-  plugins: [
-    require("tailwindcss-textshadow"),
-    require('tailwind-scrollbar')
-  ],
+  plugins: [require("tailwindcss-textshadow"), require("tailwind-scrollbar")],
 };
